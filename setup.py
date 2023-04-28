088e907044f8c534d5be23ee5a5dc35d0c428a65
sam_abyss <s.matthews@abysssolutions.com.au>
2023-04-28 15:25:55 +1000

    Add code to setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Install SAM_UI_TOOL library
+"""
+
+import os
+from setuptools import setup
+
+setup(
+    name="SAM_UI_TOOL",
+    description="UI tool for segmenting images with Metas Segment Anything Model.",
+    packages=["SAM_UI_TOOL"],
+    package_data={},
+    install_requires=[
+        "numpy",
+        "onnxruntime",
+        "opencv_contrib_python",
+        "opencv-python",
+        "Pillow",
+        "pycocotools",
+        "torch",
+        "torchvision",
+    ],
+    scripts=[],
+)
